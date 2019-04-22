69d6a246c0c1904b25e4daf6f3ebc46b13ac8085
viettranisnottaken <48340676+viettranisnottaken@users.noreply.github.com>
2019-04-23 03:50:55 +0700

    1

--- draft2js.js
+++ draft2js.js
@@ -1,6 +1,27 @@
 var backslash = "I am the \"shit\" ", backslash2 = "It\'s me, Mario";
 document.getElementById('one').innerHTML = backslash + backslash2;
 
+var txt = "I can eat bananas all day can eat bananas";
+var a = txt.slice (11, -8);
+document.getElementById('six').innerHTML = txt.substring (11, 17); //substring (begin, end) but no negative
+document.getElementById('seven').innerHTML = txt.substr (6, 3); //substr(beginning, length)
+var b = txt.toUpperCase();
+document.getElementById('three').innerHTML = a + " " + "and " + b;
+document.getElementById('four').innerHTML = txt.indexOf("eat");
+document.getElementById('five').innerHTML = txt.lastIndexOf("eat");
+
+document.getElementById('eight').innerHTML = txt;
+function replace() {
+  var txt2 = txt.replace ("bananas", "Pu$$x");
+  document.getElementById('eight').innerHTML = txt2;
+};
+function replace_reverse() {
+  document.getElementById('eight').innerHTML = txt;
+};
+
+window.alert (txt.charAt (0));
+
+
 function addition(x, y) {
   return x*y;
 };
