046bc20cb4d950e12411f219837f91cb73c0f749
viettranisnottaken <48340676+viettranisnottaken@users.noreply.github.com>
2019-04-22 23:49:59 +0700

    1

---
+++ draft2js.js
@@ -0,0 +1,8 @@
+var backslash = "I am the \"shit\" ", backslash2 = "It\'s me, Mario";
+document.getElementById('one').innerHTML = backslash + backslash2;
+
+function addition(x, y) {
+  return x*y;
+};
+
+document.getElementById('two').innerHTML = addition(4,2);
