622f25ccb3e0c22e8132d598ca903184ad468827
viettranisnottaken <48340676+viettranisnottaken@users.noreply.github.com>
2019-04-26 07:26:29 +0700

    Wheel of Fortune!

--- draft2js.js
+++ draft2js.js
@@ -37,4 +37,34 @@ var flNum = 7.7563;
 document.getElementById('hehehe').innerHTML = flNum.toFixed(0) + " " +
   flNum.toFixed(1) + " " + flNum.toFixed(2) + " " + flNum.toFixed(6);
 
-document.getElementById('eleven').innerHTML = txt;
+var bodyParts = ["Legs", "Arms", "Eyes", "TDz"];
+document.getElementById('thirteen').innerHTML = bodyParts.join(" and ");
+bodyParts.pop("Arms");
+document.getElementById('fourteen').innerHTML = bodyParts;
+
+// Wheel of fortune:
+function wheelOfFortune() {
+  var x = Math.floor(Math.random() * 7), y;
+  switch (x) {
+    case 1:
+      y = "You won: A half-consumed bottle of wine!";
+      break;
+    case 2:
+      y = "You won: A jizz-ridden sock!";
+      break;
+    case 3:
+      y ="You won: A somewhat-functional headphone!";
+      break;
+    case 4:
+      y ="You won: A guy in his 20s!";
+      break;
+    case 5:
+      y ="You won: A keyboard with two thirds of the keys ripped out!";
+      break;
+    case 6:
+      y = "You won: A teacher whose initials are TD!";
+      break;
+    default: y = "Lol u can't even win. Go cry.";
+  };
+  window.alert(y);
+};
